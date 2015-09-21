8652844cf8d3f7ce9184caf05b312daf45a00d94
Elena Caballero <elena.caballero@unitec.edu>
2015-09-20 19:25:18 -0600

    indices2

--- createIndex.cpp
+++ createIndex.cpp
@@ -8,10 +8,56 @@ struct Index{
 	std::vector<int> reference;
 };
 
+struct Cliente{
+	char idCliente[20];
+	char nombre[40];
+	char genero;
+	int idCity;
+};
+
+struct Lineas{
+  	char numero[20];
+	char idCliente[20];
+};
+
+struct Ciudad{
+  	int idCity;
+	char city[40];
+};
+
+int menu();
+
 int main(int argc, char const *argv[]){
-	
+	ifstream file;
+	int op = menu();
+	if (op == 1){
+		Cliente item;
+		file.open("cliente.bin", ifstream::binary);
+
+	}else if (op == 2){
+		Lineas item;
+		file.open("linea.bin", ifstream::binary);
+
+	}else if (op == 3){
+		Ciudad item;
+		file.open("ciudad.bin", ifstream::binary);
+		
+	} else if (op <= 0 || op > 3){
+		cout << "Ingrese una opcion existente";
+		op = menu();
+	}
 
 
 
 	return 0;
+}
+
+int menu(){
+	int op = 0;
+	cout << "Seleccione el archivo a indexar: " << endl;
+	cout << "1. Clientes" << endl;
+	cout << "2. Lineas Telefónicas" << endl;
+	cout << "3. Ciudades" << endl;
+	cin >> op;
+	return op;
 }
