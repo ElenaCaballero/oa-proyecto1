8c4c48420d430fd7a82c6a22129f057fd3273059
Elena Caballero <elena.caballero@unitec.edu>
2015-09-20 18:50:20 -0600

    indices1

--- createIndex.cpp
+++ createIndex.cpp
@@ -3,7 +3,15 @@
 #include <fstream>  
 using namespace std;
 
+struct Index{
+	std::vector<char*> key;
+	std::vector<int> reference;
+};
+
 int main(int argc, char const *argv[]){
+	
+
+
 
 	return 0;
 }
